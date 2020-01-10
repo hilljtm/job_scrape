4989aa30495c50c336dbd05313da4a932e23fb14
hilljtm <47020214+hilljtm@users.noreply.github.com>
2020-01-10 14:21:16 -0500

    monster's done and parsed

--- monster.py
+++ monster.py
@@ -8,21 +8,31 @@ page = requests.get(url)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 results = soup.find(id='SearchResults')
-print(results.prettify())
+#print(results.prettify())
 
 
 job_elems = results.find_all('section', class_='card-content')
 
-for job_elem in job_elems:
-    print(job_elem, end='\n' * 2)
+#for job_elem in job_elems:
+    #print(job_elem, end='\n' * 2)
 
 
 for job_elem in job_elems:
     title_elem = job_elem.find('h2', class_='title')
     company_elem = job_elem.find('div', class_='company')
-    location_elem = job.elem.find('div', class_='location')
-    print(title_elem)
-    print(company_elem)
-    print(location_elem)
+    location_elem = job_elem.find('div', class_='location')
+    if None in (title_elem, company_elem, location_elem):
+        continue
+    print(title_elem.text.strip())
+    print(company_elem.text.strip())
+    print(location_elem.text.strip())
     print()
 
+
+python_jobs = results.find_all('h2', string=lambda text: 'python' in text.lower())
+print(len(python_jobs))
+
+for p_job in python_jobs:
+    link = p_job.find('a')['href']
+    print(p_job.text.strip())
+    print(f'Apply here: {link}\n')
