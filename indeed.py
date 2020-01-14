6084d96c7546a80396e71b2e863f016c1c16837d
hilljtm <47020214+hilljtm@users.noreply.github.com>
2020-01-14 15:27:48 -0500

    need halp, plz fix

--- indeed.py
+++ indeed.py
@@ -6,20 +6,30 @@ page = requests.get(url)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 results = soup.find(id='resultsCol')
-#print(results.prettify())
+print(results.prettify())
 
 # THIS IS CAUSING NO POSTINGS TO SHOW UP, SPECIFICALLY THE SECTION AND CLASS ARE WRONG!!!!!!
-#job_elems = results.find_all('section', class_='jobsearch-SerpJobCard')
+job_elems = results.find_all('id', class_='resultsCol')
+
 
 for job_elem in job_elems:
     title_elem = job_elem.find('div', class_='title')
-    company_elem = job_elem.find('div', class_='company')
+    company_elem = job_elem.find('div', class_='sjcl')
     location_elem = job_elem.find('div', class_='recJobLoc')
-    salary_elem = job_elem.find('div', class_='salarySnippetholisticSalary')
+    salary_elem = job_elem.find('div', class_='salarySnippet.holisticSalary')
+    if None in (title_elem, company_elem, location_elem, salary_elem):
+        continue
     print(title_elem.text.strip())
     print(company_elem.text.strip())
     print(location_elem.text.strip())
     print(salary_elem.text.strip())
     print()
-    
 
+
+#python_jobs = results.find_all('div', string=lambda text: 'python' in text.lower())
+#print(len(python_jobs))
+
+#for p_job in python_jobs:
+#    link = p_job.find('a')['href']
+#    print(p_job.text.strip())
+#    print(f'Apply here: {link}\n')
