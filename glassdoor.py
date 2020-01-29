a6fdf2f4fb52fa14ed90f37b20c48aa86945eac3
hilljtm <47020214+hilljtm@users.noreply.github.com>
2020-01-29 15:53:22 -0500

    need to add more sites, maybe include data analyst positions, also need to export the return to a csv

--- glassdoor.py
+++ glassdoor.py
@@ -4,5 +4,23 @@ from bs4 import BeautifulSoup
 url = 'https://www.glassdoor.com/Job/jobs.htm?suggestCount=0&suggestChosen=true&clickSource=searchBtn&typedKeyword=Python&sc.keyword=Python+Programmer&locT=C&locId=1145013&jobType='
 
 page = requests.get(url)
-print(page)
 
+
+soup = BeautifulSoup(page.content, 'html.parser')
+results = soup.find(id='')
+
+
+job_elems = results.find_all('section', class_='')
+
+for job_elem in job_elems:
+    title_elem = job_elem.find()
+    company_elem = job_elem.find()
+    location_elem = job_elem.find()
+    if None in (title_elem, company_elem, location_elem):
+        continue
+    print(title_elem.text.strip())
+    print(company_elem.text.strip())
+    print(location_elem.text.strip())
+    print()
+
+python_jobs = results.find_all()
